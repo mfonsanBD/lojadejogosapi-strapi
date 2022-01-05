065dff3e6da2b4be74d2669ce1d925e754f34b42
Mike Santos <iddeiasimplesmike@gmail.com>
2022-01-05 20:35:53 -0300

    correção em configurações para produção

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'meusite'),
-        username: env('DATABASE_USERNAME', 'meusite'),
-        password: env('DATABASE_PASSWORD', 'Heitor28112020*/'),
+        database: env('DATABASE_NAME', 'wongames'),
+        username: env('DATABASE_USERNAME', 'wongames'),
+        password: env('DATABASE_PASSWORD', 'wongames123'),
         ssl: {
           rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
         },
