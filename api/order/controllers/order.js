099e82df0fb884b5468c683ec3974b265257c27b
Mike Santos <iddeiasimplesmike@gmail.com>
2022-02-23 11:32:45 -0300

    implementação da rota e do controller de criação de intenção de pagamento e integração com o stripe

---
+++ api/order/controllers/order.js
@@ -0,0 +1,54 @@
+'use strict';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = {
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body;
+
+    let games = [];
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validatedGame = await strapi.services.game.findOne({
+          id: game.id
+        })
+
+        if(validatedGame) {
+          games.push(validatedGame)
+        }
+      })
+    );
+
+    if(!games.length) {
+      ctx.response.status = 404;
+      return {
+        error: "No valid games found!"
+      };
+    }
+
+    const total = games.reduce((acc, game) => {
+      return acc + game.price
+    }, 0);
+
+    if(total === 0) {
+      return {
+        freeGames: true
+      };
+    }
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total*100,
+        currency: 'usd',
+        automatic_payment_methods: {enabled: true},
+        metadata: { integration_check: "accept_a_payment" }
+      });
+
+      return paymentIntent;
+    } catch (error) {
+      return {
+        error: error.raw.message
+      }
+    }
+  }
+};
